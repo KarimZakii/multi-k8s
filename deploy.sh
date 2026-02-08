8e43d5fc9503add08d978d043e6f811e3a4b25c6
KarimZakii <karimzaki686@gmail.com>
2026-02-09 01:18:39 +0200

    update deployment configurations and scripts for GKE and Docker images

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+# docker build -t karimzaki98/complex-server -f ./server/Dockerfile ./server
+# docker build -t karimzaki98/multi-client -f ./client/Dockerfile ./client
+# docker build -t karimzaki98/multi-worker -f ./worker/Dockerfile ./worker
+# docker push karimzaki98/complex-server
+# docker push karimzaki98/multi-client
+# docker push karimzaki98/multi-worker
+echo "Working fine"
