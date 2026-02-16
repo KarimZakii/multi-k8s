a5649abdae55e6b47fbbbc600959a95cfc5e1860
karim.zaki <karim1.zaki@orange.com>
2026-02-16 18:57:29 +0200

    DEPLOYING TO GCP YARAB YSHTAGHALL

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,18 @@
-# docker build -t karimzaki98/complex-server -f ./server/Dockerfile ./server
-# docker build -t karimzaki98/multi-client -f ./client/Dockerfile ./client
-# docker build -t karimzaki98/multi-worker -f ./worker/Dockerfile ./worker
-# docker push karimzaki98/complex-server
-# docker push karimzaki98/multi-client
-# docker push karimzaki98/multi-worker
-echo "Working fine"
+docker build -t karimzaki98/complex-server:latest -t karimzaki98/complex-server:$SHA -f ./server/Dockerfile ./server
+docker build -t karimzaki98/multi-client:latest -t karimzaki98/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t karimzaki98/multi-worker:latest -t karimzaki98/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push karimzaki98/complex-server:latest
+docker push karimzaki98/multi-client:latest
+docker push karimzaki98/multi-worker:latest
+
+docker push karimzaki98/complex-server:$SHA
+docker push karimzaki98/multi-client:$SHA
+docker push karimzaki98/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=karimzaki98/complex-server:$SHA
+kubectl set image deployments/client-deployment client=karimzaki98/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=karimzaki98/multi-worker:$SHA
+
+echo "All Good"
